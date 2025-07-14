457d68f0ce13a27b98691113c625fb0c9457e055
Renato Soares <renatosoarespro@hotmail.com>
2025-07-14 18:43:09 -0300

    feat: decreases the percentage

--- check_memory_limit.sh
+++ check_memory_limit.sh
@@ -3,8 +3,8 @@
 while true; do
     MEM_USO=$(free | awk '/Mem:/ {printf "%.0f", $3/$2 * 100}')
     
-    if [ "$MEM_USO" -gt 85 ]; then
-        notify-send "Alerta de Memória" "Uso de memória acima de 85%: ${MEM_USO}%"
+    if [ "$MEM_USO" -gt 80 ]; then
+        notify-send "Alerta de Memória" "Uso de memória acima de 80%: ${MEM_USO}%"
     fi
 
     sleep 30
