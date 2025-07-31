91a331fd1a4a342e9ef46b853ed4d8b0711deaf6
Renato Soares <renatosoarespro@hotmail.com>
2025-07-31 11:35:53 -0300

    feat: add command to generate password

--- php_artisan.sh
+++ php_artisan.sh
@@ -3,20 +3,27 @@
 php_artisan() {
     command=$1
 
+    echo "Entering the project directory..."
+    cd "$RITS_PAY_API" || { echo "Directory not found!"; exit 1; }
+
     case "$command" in
     cpf)
-        echo "Entering the project directory..."
-        cd "$RITS_PAY_API" || { echo "Directory not found!"; exit 1; }
-
         echo "call fake cpf document"
         ./vendor/bin/sail artisan tinker --execute="echo (\Faker\Factory::create('pt_BR'))->cpf . PHP_EOL;"
         ;;
+    
+    pwd)
+        echo "Generate password"
+        ./vendor/bin/sail artisan tinker --execute="echo str()->password() . PHP_EOL;"
+        ;;
+
     sandbox)
         echo "sandbox..."
         ./vendor/bin/sail artisan sandbox
         ;;
+
     *)
-        echo "Uso: $0 {cpf|sandbox}"
+        echo "Uso: $0 {cpf|sandbox|pwd}"
         ;;
     esac
 }
